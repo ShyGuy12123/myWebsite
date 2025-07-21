47fa9c505d914b3aa49f1f5e5a3239a7e876dadb
ShyGuy12123 <Pokeman4815162342@gmail.com>
2025-07-21 16:02:40 -0400

    things

---
+++ hamburger.js
@@ -0,0 +1,18 @@
+function tsw_expand_navmenu() {
+
+    document.getElementById('tswcssbuttons').style.display = 'block';
+
+    document.getElementById('tswhamburger').style.display = 'none';
+
+    document.getElementById('tswhamburger2').style.display = 'block';
+
+}
+function tsw_contract_navmenu() {
+
+    document.getElementById('tswcssbuttons').style.display = 'none';
+
+    document.getElementById('tswhamburger').style.display = 'block';
+
+    document.getElementById('tswhamburger2').style.display = 'none';
+
+}
